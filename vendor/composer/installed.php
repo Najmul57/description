a5eabeac750f873f1397249ca2c607ad7e4bcb15
najmul57 <najmulcse247@gmail.com>
2024-07-14 18:13:47 +0600

    some thing design done

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/documentation',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8a617da07524c1516862795f2613b7f42b12bea5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/documentation' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8a617da07524c1516862795f2613b7f42b12bea5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
